ab0cf145cb7bd87ff1d56495c29a0e96e284c3ce
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-12-02 21:35:45 +1100

    Update InfoBot.sol

--- contracts/InfoBot.sol
+++ contracts/InfoBot.sol
@@ -1,20 +1,36 @@
 pragma solidity 0.6.6;
 
-contract InfoBot {
+// ----------------------------------------------------------------------------
+//
+//                      (c) The BotNet Project 2020
+//
+// ----------------------------------------------------------------------------
 
-    uint totalUsersHoldingCredits;            // how many users hold credits
+contract InfoBot {
     
-    mapping(address => userInfo) users;       // user address assigned to personal info sheet
+    uint _enigmaFee;                             // Enigma entrance fee
+    uint _enigmaBalance;                         // total funds in Enigma
+    uint totalEnigmaParticipants;                // total enigma participants
+    address[] enigmaParticipants;                // addresses participating in Enigma
+
+    uint totalUsersHoldingCredits;               // amount of users holding credits
+
+    mapping(address => userInfo) users;          // user address assigned to personal info sheet
 
     struct userInfo {
-        uint creditBalance;                   // credit balance
-        uint encryptedBalance;                // encrypted balance
-        uint _initalEncryptedBalance;         // inital encrypted balance deposit for each hack
-        uint toTransfer;                      // amount to transfer
-        bool holdsCredits;                    // user is/isn't holding credits
-        bool isHacking;                       // hacking status
-        uint currentHackTime;                 // current hacking time progress
-        uint hackCompletionTime;              // hacking completion time
-        mapping (address => uint256) allowed; // credits allowed to transfer  // 
+        uint creditBalance;                      // credit balance
+        uint encryptedBalance;                   // encrypted balance
+        uint _initalEncryptedBalance;            // inital encrypted balance deposit for each hack
+        uint toTransfer;                         // amount to transfer
+        bool holdsCredits;                       // user is/isn't holding credits
+        bool isHacking;                          // hacking status
+        uint hackCompletionTime;                 // hacking completion time
+        mapping (address => uint256) allowed;    // credits allowed to transfer
+        
+        bool inEnigma;
+        uint joinedEnigma;
+        uint leftEnigma;
+        // uint timeUntilEnigmaRecipient;        // future implimentation, maybe
+        // bool enigmaRecipient;                 // future implimentation, maybe
     }
 }
