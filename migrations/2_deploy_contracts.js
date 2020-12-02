f0e94f601662cb43b561536a0c11c50ed79d1cc7
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-12-02 16:41:35 +1100

    Update 2_deploy_contracts.js
    [added] InfoBot + DistributionBot

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const Credits = artifacts.require("Credits");
 const CreditsInterface = artifacts.require("CreditsInterface");
+const DistributionBot = artifacts.require("DistributionBot");
 const ERC20 = artifacts.require("ERC20");
 const HackerBot = artifacts.require("HackerBot");
+const InfoBot = artifacts.require("InfoBot");
 const Permissioned = artifacts.require("Permissioned");
 const SafeMath = artifacts.require("SafeMath");
 const SwapBot = artifacts.require("SwapBot");
@@ -13,10 +15,16 @@ module.exports = function (deployer) {
   deployer.deploy(CreditsInterface);
 };
 module.exports = function (deployer) {
+  deployer.deploy(DistributionBot);
+};
+module.exports = function (deployer) {
   deployer.deploy(ERC20);
 };
 module.exports = function (deployer) {
   deployer.deploy(HackerBot);
+};
+module.exports = function (deployer) {
+  deployer.deploy(InfoBot);
 };
 module.exports = function (deployer) {
   deployer.deploy(Permissioned);
