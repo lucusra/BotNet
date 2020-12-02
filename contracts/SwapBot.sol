adc69b66ab2290a057e6132ebab3ef97d7e91aee
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-12-02 16:37:34 +1100

    Update SwapBot.sol
    [updated] old mappings with userInfo struct members

--- contracts/SwapBot.sol
+++ contracts/SwapBot.sol
@@ -9,6 +9,7 @@ import "./Credits.sol";
 // ----------------------------------------------------------------------------
 
 contract SwapBot is Credits {
+
     uint _conversionRate;
 
     function exchange_Eth_To_Credits() public payable pauseFunction returns(bool success) {
@@ -16,18 +17,26 @@ contract SwapBot is Credits {
         require(msg.sender.balance > 0 wei, "insufficient enough funds");
         _totalSupplyHeld = _totalSupplyHeld.add((msg.value.mul(_conversionRate)).div(1000000000000000000));
         _totalSupply = _totalSupply.add((msg.value.mul(_conversionRate)).div(1000000000000000000));
-        creditBalances[msg.sender] = creditBalances[msg.sender].add((msg.value.mul(_conversionRate)).div(1000000000000000000));
+        users[msg.sender].creditBalance = users[msg.sender].creditBalance.add((msg.value.mul(_conversionRate)).div(1000000000000000000));
+        if (users[msg.sender].creditBalance != 0 && users[msg.sender].holdsCredits == false) {
+            users[msg.sender].holdsCredits = true;
+            totalUsersHoldingCredits = totalUsersHoldingCredits.add(1);
+        }
         return true;
     }
     function exchange_Credits_To_Eth(uint creditsAmount) public pauseFunction returns(bool success) {
-        require(creditBalances[msg.sender] != 0, "balance is empty, unable to withdraw");
-        require(creditBalances[msg.sender] >= creditsAmount, "insufficient funds to withdraw");
+        require(users[msg.sender].creditBalance != 0, "balance is empty, unable to withdraw");
+        require(users[msg.sender].creditBalance >= creditsAmount, "insufficient funds to withdraw");
         require(creditsAmount != 0, "unable to withdraw 0");
         _totalSupply = _totalSupply.sub(creditsAmount);
         _totalSupplyHeld = _totalSupplyHeld.sub(creditsAmount);
-        creditBalances[msg.sender] = creditBalances[msg.sender].sub(creditsAmount);
+        users[msg.sender].creditBalance = users[msg.sender].creditBalance.sub(creditsAmount);
         creditsAmount = creditsAmount.mul(1000000000000000000);
         msg.sender.transfer(creditsAmount.div(_conversionRate));
+        if (users[msg.sender].creditBalance == 0 && users[msg.sender].holdsCredits == true) {
+            users[msg.sender].holdsCredits = false;
+            totalUsersHoldingCredits = totalUsersHoldingCredits.sub(1);
+        }
         return true;
     }
     function setConversionRate(uint newRate) public onlyOwner returns(uint rate) {
