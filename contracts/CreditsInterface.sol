1ac7837e942a20ebb17326013b86cebdefd0388f
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-12-02 21:37:04 +1100

    Update CreditsInterface.sol

--- contracts/CreditsInterface.sol
+++ contracts/CreditsInterface.sol
@@ -2,9 +2,9 @@ pragma solidity 0.6.6;
 
 import "./ERC20.sol";
 
-/// @notice CreditsInterface = ERC20 + mint + burn
+/// @notice CreditsInterface = ERC20 + mint + burn. (c) The BotNet Project 2020
 
 interface CreditsInterface is ERC20 {
-	function mint(address tokenOwner, uint tokens) external returns (bool success);
-	function burn(uint tokens) external returns (bool success);
+	function generateCredits(address tokenOwner, uint tokens) external returns (bool success);
+	function deleteCredits(uint tokens) external returns (bool success);
 }
