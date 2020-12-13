ecf2718a080e1564efa457d028663918eea06df3
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-12-14 09:49:38 +1100

    Update ERC20.sol

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -5,14 +5,14 @@ interface ERC20 {
     function totalSupply() external view returns (uint);
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    function transfer(address to, uint tokens) external returns (bool success);
-    function approve(address spender, uint tokens) external returns (bool success);
-    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+    function transfer(address _to, uint _value) external returns (bool success);
+    function approve(address _spender, uint _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint _value) external returns (bool success);
 
 // Are optional 
-    // function symbol() external view returns (string memory);
-    // function name() external view returns (string memory);
-    // function decimals() external view returns (uint8);
+     function symbol() external view returns (string memory);
+     function name() external view returns (string memory);
+     function decimals() external view returns (uint8);
 
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
