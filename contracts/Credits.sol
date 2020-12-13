90cf9868f9b94813a576c2f1c89f73d5f6355534
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-12-14 09:49:06 +1100

    Update Credits.sol

--- contracts/Credits.sol
+++ contracts/Credits.sol
@@ -44,44 +44,48 @@ contract Credits is CreditsInterface, Permissioned, InfoBot {
     function balanceOf(address tokenOwner) override external view returns (uint balance) {
     	return users[tokenOwner].creditBalance;
     }
-    function transfer(address to, uint tokens) override external pauseFunction returns (bool success) {
-        require(to != msg.sender, "unable to transfer credits to yourself");
-        require(users[msg.sender].creditBalance >= tokens, "insufficient credits");
-    	users[msg.sender].creditBalance = users[msg.sender].creditBalance.sub(tokens);
-    	users[to].creditBalance = users[to].creditBalance.add(tokens);
-    	emit Transfer(msg.sender, to, tokens);
+    function transfer(address _to, uint _value) override external pauseFunction returns (bool success) {
+        require(users[msg.sender].creditBalance >= _value);
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal pauseFunction {
+        require(_to != address(0));
+        users[_from].creditBalance = users[_from].creditBalance.sub(_value);
+        users[_to].creditBalance = users[_to].creditBalance.add(_value);
+        emit Transfer(msg.sender, _to, _value);
+    }
+    
+    function approve(address _spender, uint _value) override external pauseFunction returns (bool success) {
+        require(_spender != address(0));
+    	users[msg.sender].allowance[_spender] = _value;
+    	emit Approval(msg.sender, _spender, _value);
     	return true;
     }
-    function approve(address spender, uint tokens) override external pauseFunction returns (bool success) {
-        require(spender != msg.sender, "unable to approve tokens to yourself");
-    	users[msg.sender].allowed[spender] = tokens;
-    	emit Approval(msg.sender, spender, tokens);
-    	return true;
-    }
-    function transferFrom(address from, address to, uint tokens) override external pauseFunction returns (bool success) {
-        require(from != msg.sender && to != msg.sender, "unable to transfer to yourself");
-        users[from].creditBalance = users[from].creditBalance.sub(tokens);
-        users[from].allowed[msg.sender] = users[from].allowed[msg.sender].sub(tokens);
-        users[to].creditBalance = users[to].creditBalance.add(tokens);
-        emit Transfer(from, to, tokens);
+    function transferFrom(address _from, address _to, uint _value) override external pauseFunction returns (bool success) {
+        require(_value <= users[_from].creditBalance);
+        require(_value <= users[_from].allowance[msg.sender]);
+        users[_from].allowance[msg.sender] = users[_from].allowance[msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
         return true;
     }
     function allowance(address tokenOwner, address spender) override external pauseFunction view returns (uint remaining) {
-        return users[tokenOwner].allowed[spender];
+        return users[tokenOwner].allowance[spender];
     }
-    function generateCredits(address tokenOwner, uint tokens) override external onlyOwner returns (bool success) {
-        users[tokenOwner].creditBalance = users[tokenOwner].creditBalance.add(tokens);
-        totalCreditsSupply = totalCreditsSupply.add(tokens);
-        totalCreditsHeld = totalCreditsHeld.add(tokens);
-        emit Transfer(address(0), tokenOwner, tokens);
+    function generateCredits(address tokenOwner, uint _amount) override external onlyOwner returns (bool success) {
+        users[tokenOwner].creditBalance = users[tokenOwner].creditBalance.add(_amount);
+        totalCreditsSupply = totalCreditsSupply.add(_amount);
+        totalCreditsHeld = totalCreditsHeld.add(_amount);
+        emit Transfer(address(0), tokenOwner, _amount);
         return true;
     }
-    function deleteCredits(uint tokens) override external onlyOwner returns (bool success) {
-        require(totalCreditsSupply >= tokens && totalCreditsHeld > tokens, "unable to burn held tokens");
+    function deleteCredits(uint _amount) override external onlyOwner returns (bool success) {
+        require(totalCreditsSupply >= _amount && totalCreditsHeld > _amount, "unable to burn held tokens");
         require(totalCreditsSupply >= totalCreditsHeld, "unable to burn held tokens");
-        users[msg.sender].creditBalance = users[msg.sender].creditBalance.sub(tokens);
-        totalCreditsSupply = totalCreditsSupply.sub(tokens);
-        emit Transfer(msg.sender, address(0), tokens);
+        users[msg.sender].creditBalance = users[msg.sender].creditBalance.sub(_amount);
+        totalCreditsSupply = totalCreditsSupply.sub(_amount);
+        emit Transfer(msg.sender, address(0), _amount);
         return true;
     }
 }
