4298bc75c5bf89a31dfe5014e3fbad6386c2b075
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-12-14 20:33:48 +1100

    Update SwapBot.sol
    Complete revamp of SwapBot

--- contracts/SwapBot.sol
+++ contracts/SwapBot.sol
@@ -2,49 +2,136 @@ pragma solidity 0.6.6;
 
 import "./Credits.sol";
 
-// ----------------------------------------------------------------------------
-//
-//                      (c) The BotNet Project 2020
-//
-// ----------------------------------------------------------------------------
+contract SwapBot {
+    using SafeMath for uint256;
 
-contract SwapBot is Credits {
+    address public feeAccount;                                                  // the account that receives exchange fees
+    uint256 public feePercent;                                                  // the fee percentage
+    address constant ETHER = address(0);                                        // store Ether in credits mapping with blank address
+    mapping(address => mapping(address => uint256)) public creditsAvailable;    // ether & credits available
+    mapping(uint256 => _Order) public orders;                                   // storage of the different orders
+    uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
+    mapping(uint256 => bool) public orderFilled;
 
-    uint _conversionRate;
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Order(
+        uint256 id,
+        address user,       
+        address tokenGet, 
+        uint256 amountGet,     
+        address tokenGive,  
+        uint256 amountGive,    
+        uint256 timestamp
+    );
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+    event Trade(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        address userFill,
+        uint256 timestamp
+    );
 
-    function exchange_Eth_To_Credits() public payable pauseFunction returns(bool success) {
-        require(msg.value > 0 wei, "balance is empty, unable to deposit");
-        require(msg.sender.balance > 0 wei, "insufficient enough funds");
-        totalCreditsHeld = totalCreditsHeld.add((msg.value.mul(_conversionRate)).div(1000000000000000000));
-        totalCreditsSupply = totalCreditsSupply.add((msg.value.mul(_conversionRate)).div(1000000000000000000));
-        users[msg.sender].creditBalance = users[msg.sender].creditBalance.add((msg.value.mul(_conversionRate)).div(1000000000000000000));
-        if (users[msg.sender].creditBalance != 0 && users[msg.sender].holdsCredits == false) {
-            users[msg.sender].holdsCredits = true;
-            totalUsersHoldingCredits = totalUsersHoldingCredits.add(1);
-        }
-        return true;
+
+    // template to add orders
+    struct _Order {
+        uint256 id; 
+        address user;           // the person who create the order
+        address tokenGet;       // token user wants to purchase
+        uint256 amountGet;      // the amount of the token they want to get
+        address tokenGive;      // the token they're going to use in the trade
+        uint256 amountGive;     // the amount of the token they're going to trade
+        uint256 timestamp;      // time of when the order was created
     }
-    function exchange_Credits_To_Eth(uint creditsAmount) public pauseFunction returns(bool success) {
-        require(users[msg.sender].creditBalance != 0, "balance is empty, unable to withdraw");
-        require(users[msg.sender].creditBalance >= creditsAmount, "insufficient funds to withdraw");
-        require(creditsAmount != 0, "unable to withdraw 0");
-        totalCreditsSupply = totalCreditsSupply.sub(creditsAmount);
-        totalCreditsHeld = totalCreditsHeld.sub(creditsAmount);
-        users[msg.sender].creditBalance = users[msg.sender].creditBalance.sub(creditsAmount);
-        creditsAmount = creditsAmount.mul(1000000000000000000);
-        msg.sender.transfer(creditsAmount.div(_conversionRate));
-        if (users[msg.sender].creditBalance == 0 && users[msg.sender].holdsCredits == true) {
-            users[msg.sender].holdsCredits = false;
-            totalUsersHoldingCredits = totalUsersHoldingCredits.sub(1);
-        }
-        return true;
+
+    constructor (address _feeAccount, uint256 _feePercent) public {
+        feeAccount = _feeAccount;
+        feePercent = _feePercent;
     }
-    function setConversionRate(uint newRate) public onlyOwner returns(uint rate) {
-        require(_conversionRate != newRate, "unable to change to the same rate");
-        _conversionRate = newRate;
-        return _conversionRate;
-    } 
-    function conversionRate() public view returns(uint rate) {
-        return _conversionRate;
+
+    // Fallback: reverts if Ether is sent to this smart contract by mistake
+    fallback() external {
+        revert();
+    }
+
+    function depositEther() payable public {
+        creditsAvailable[ETHER][msg.sender] = creditsAvailable[ETHER][msg.sender].add(msg.value);
+        emit Deposit(ETHER, msg.sender, msg.value, creditsAvailable[ETHER][msg.sender]);
+    }
+
+    function withdrawEther(uint _amount) public {
+        require(creditsAvailable[ETHER][msg.sender] >= _amount);
+        creditsAvailable[ETHER][msg.sender] = creditsAvailable[ETHER][msg.sender].sub(_amount);
+        msg.sender.transfer(_amount);
+        emit Withdraw(ETHER, msg.sender, _amount, creditsAvailable[ETHER][msg.sender]);
+    }
+
+    function depositCredits(address _token, uint _amount) public {
+        require(_token != ETHER);
+        require(Credits(_token).transferFrom(msg.sender, address(this), _amount));
+        creditsAvailable[_token][msg.sender] = creditsAvailable[_token][msg.sender].add(_amount);
+        emit Deposit(_token, msg.sender, _amount, creditsAvailable[_token][msg.sender]);
+    }
+
+    function withdrawCredits(address _token, uint256 _amount) public {
+        require(_token != ETHER);
+        require(creditsAvailable[_token][msg.sender] >= _amount);
+        creditsAvailable[_token][msg.sender] = creditsAvailable[_token][msg.sender].sub(_amount);
+        require(Credits(_token). transfer(msg.sender, _amount));
+        emit Withdraw(_token, msg.sender, _amount, creditsAvailable[_token][msg.sender]);
+    }
+
+    function balanceOf(address _token, address _user) public view returns (uint256) {
+        return creditsAvailable[_token][_user];
+    }
+
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+        orderCount = orderCount.add(1);
+        orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+        emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    }
+
+    function cancelOrder(uint256 _id) public {
+        _Order storage _order = orders[_id];
+        require(address(_order.user) == msg.sender);        // order must be the user's
+        require(_order.id == _id);                          // the order must exist
+        orderCancelled[_id] = true;
+        emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, now);
+    }
+
+    function fillOrder(uint256 _id) public {
+        require(_id > 0 && _id <= orderCount);
+        require(!orderFilled[_id]);
+        require(!orderCancelled[_id]);
+        _Order storage _order = orders[_id];
+        _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+        orderFilled[_order.id] = true;
+    }
+
+    function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+        // Fee paid by the user that fills the order, a.k.a. msg.sender.
+        uint256 _feeAmount = _amountGet.mul(feePercent).div(100); // 10% fee
+
+        // Execute trade
+        creditsAvailable[_tokenGet][msg.sender] = creditsAvailable[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
+        creditsAvailable[_tokenGet][_user] = creditsAvailable[_tokenGet][_user].add(_amountGet);
+        creditsAvailable[_tokenGet][feeAccount] = creditsAvailable[_tokenGet][feeAccount].add(_feeAmount);
+        creditsAvailable[_tokenGive][_user] = creditsAvailable[_tokenGive][_user].sub(_amountGive);
+        creditsAvailable[_tokenGive][msg.sender] = creditsAvailable[_tokenGive][msg.sender].add(_amountGive);
+
+        emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, now);
     }
 }
