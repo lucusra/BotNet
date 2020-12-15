472f65c61119fbb2cce591675ab663cae45e6cc7
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-12-15 17:20:28 +1100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,37 +1,31 @@
-const Credits = artifacts.require("Credits");
-const CreditsInterface = artifacts.require("CreditsInterface");
-const DistributionBot = artifacts.require("DistributionBot");
-const ERC20 = artifacts.require("ERC20");
-const HackerBot = artifacts.require("HackerBot");
-const InfoBot = artifacts.require("InfoBot");
-const Permissioned = artifacts.require("Permissioned");
 const SafeMath = artifacts.require("SafeMath");
+const Permissioned = artifacts.require("Permissioned");
+const InfoBot = artifacts.require("InfoBot");
+const ERC20 = artifacts.require("ERC20");
+const CreditsInterface = artifacts.require("CreditsInterface");
+const Credits = artifacts.require("Credits");
+const CreditsITO = artifacts.require("CreditsITO");
+const DistributionBot = artifacts.require("DistributionBot");
+const HackerBot = artifacts.require("HackerBot");
 const SwapBot = artifacts.require("SwapBot");
 
 module.exports = function (deployer) {
-  deployer.deploy(Credits);
-};
-module.exports = function (deployer) {
-  deployer.deploy(CreditsInterface);
-};
-module.exports = function (deployer) {
-  deployer.deploy(DistributionBot);
-};
-module.exports = function (deployer) {
+  deployer.deploy(SafeMath);
+  deployer.deploy(Permissioned);
+  deployer.deploy(InfoBot);
   deployer.deploy(ERC20);
-};
-module.exports = function (deployer) {
+  deployer.deploy(CreditsInterface);
+  deployer.deploy(Credits);
+  deployer.deploy(CreditsITO);
+  deployer.deploy(DistributionBot);
   deployer.deploy(HackerBot);
 };
-module.exports = function (deployer) {
-  deployer.deploy(InfoBot);
-};
-module.exports = function (deployer) {
-  deployer.deploy(Permissioned);
-};
-module.exports = function (deployer) {
-  deployer.deploy(SafeMath);
-};
-module.exports = function (deployer) {
-  deployer.deploy(SwapBot);
+
+module.exports = async function (deployer) {
+  const accounts = await web3.eth.getAccounts()
+
+  const feeAccount = accounts[0]
+  const feePercent = 10
+
+  await deployer.deploy(SwapBot, feeAccount, feePercent);
 };
