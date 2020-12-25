cd1172592cb922610a7ee6f176c7e585e1fdb57f
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2020-12-26 01:13:25 +1100

    Update CreditsITO.sol
    added: dev fund withdrawal + dev timelock
    need to fix transfers*

--- contracts/CreditsITO.sol
+++ contracts/CreditsITO.sol
@@ -10,38 +10,54 @@ import "./Credits.sol";
 // TO DO...
 // [X] MAKE INTO A SLOW DRIP INSTEAD OF EVERYONE GETS AT ONCE (i.e. 25% at the start of each month for 4 months)
 // [X] MAKE TOKENS CLAIMABLE FOR CREDITS  
+// [ ] MAKE MODIFIER "SOLD OUT" FOR REMAINING CREDIBYTES REMAINING, THAT ARE ABLE TO BE PURCHASED
+// [ ] MAKE A CREDITS TOTAL SUPPLY 
+// [ ] FIX TRANSFERFROM & TRANSFER FUNCTION SO DEVS CANT DRAIN SUPPLY
 
 contract CreditsITO is Credits {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    Credits credits;                                      // The token being sold
+  Credits credits;                                      // The token being sold
 
-    address payable ito_Collector;                        // Where eth funds are transfered to
-    uint256 public conversionRate_EthToCredibytes;        // How many credibytes a buyer gets per eth.
-    uint256 public conversionRate_CredibytesToCredits;    // How many credits per credibytes (always a multiplier)
-    uint256 public totalEthRaised;                        // Amount of eth raised
-    uint256 public minEthRequirement;                     // minimum amount of eth required to buy credibytes
+  uint256 public conversionRate_EthToCredibytes;        // How many credibytes a buyer gets per eth.
+  uint256 public conversionRate_CredibytesToCredits;    // How many credits per credibytes (always a multiplier)
+  uint256 public totalEthRaised;                        // Amount of eth raised
+  uint256 public minEthRequirement;                     // minimum amount of eth required to buy credibytes
+
+  address payable itoContract = address(this);          // the address that holds the credits & credibytes total supply
+  uint256 public remaining_credibyteSupply;             // the remaining credibytes that are available for purchase
+
+  uint256 public itoTotalParticipants;                  // the amount of ito user participants 
     
-    address payable itoContract = address(this);
-    uint256 public remaining_credibyteSupply;
-
-    address[] public participants;
     
-    /// @notice Event for token purchase logging
-    event CredibytesPurchase(
-      address indexed purchaser,     // who paid for the credits
-      address indexed beneficiary,   // who received the credits
-      uint256 ethAmount,             // ethers paid for credits
-      uint256 credibyteAmount        // amount of credits purchased
-    );
-    event CredibytesRedeption(
-      address indexed redeemer,
-      uint256 amount,
-      uint256 date
-    );
+  event CredibytesPurchase(                             // Event for credibytes purchase logging
+    address indexed purchaser,                          // who paid for the credits
+    address indexed beneficiary,                        // who received the credits
+    uint256 ethAmount,                                  // ethers paid for credits
+    uint256 credibyteAmount                             // amount of credits purchased
+  );
+  event CredibytesRedemption(                           // Event for credibytes redemption
+    address indexed redeemer,                           // user redeeming
+    uint256 indexed creditsAmount,                      // amount of credits getting redeemed from credibytes
+    uint256 time                                        // time of redemption
+  );    
+  event developerEthWithdrawal(                         // event for developer withdrawing eth
+    address indexed to,                                 // address the developer is sending the eth to
+    uint256 indexed amount,                             // the amount of eth being sent
+    uint256 indexed timeOfWithdrawal                    // the block timestamp of the withdrawal
+  );
+  event ethDepositToDeveloper(                          // event for developer receiving eth
+    address indexed from,                               // address that sent the eth
+    uint256 indexed amount,                             // the amount of eth being sent
+    uint256 indexed timeOfDeposit                       // the block timestamp of the deposit
+  );
+  event developerCreditsWithdrawal(                     // event for developer withdrawing credits (helps users prepare for a dump if need be)
+    address indexed to,                                 // the address receiving the credits
+    uint256 indexed amount,                             // the amount of credits being withdrawn
+    uint256 indexed timeOfWithdrawal                    // the block timestamp of the withdrawal
+  );
 
-   constructor(address payable _itoCollector) {
-    ito_Collector = _itoCollector;
+  constructor() {
     deploymentDate = block.timestamp;
     timelockActivationDate = deploymentDate + 4 weeks;
     hasFinalised = false;
@@ -53,10 +69,10 @@ contract CreditsITO is Credits {
     remaining_credibyteSupply = users[itoContract].creditBalance.div(conversionRate_CredibytesToCredits);
   }
 
+
 //  ----------------------------------------------------
 //                      Dashboard 
 //  ----------------------------------------------------
-
 
   /// @notice Allows owner to update the eth to credibytes conversion rate.
   /// @param _newRate new eth to credibytes conversion rate.
@@ -76,18 +92,18 @@ contract CreditsITO is Credits {
       minEthRequirement = _newMinimumRequirement;
   }
 
-  /// @notice Allows owner to finish the ITO before the timelock.
-  function finalise() public onlyOwner {
+  function finalise() public onlyOwner {    // Allows owner to finish the ITO before the timelock.
     hasFinalised = true;
   }
 
+
+
 //  ----------------------------------------------------
-//                      Timelock 
+//                      ITO Timelock 
 //  ----------------------------------------------------
 
-  // The block when the contract locks/stops functioning.
-  uint256 public timelockActivationDate;
-  uint256 public deploymentDate;
+  uint256 public deploymentDate;              // The block when the timer begins counting from.
+  uint256 public timelockActivationDate;      // The block when the contract locks/stops functioning.
 
   bool public hasFinalised;
 
@@ -99,7 +115,8 @@ contract CreditsITO is Credits {
       );
     _;
   }
-
+  
+  // Allows the 
   modifier canRedeem {
     require(
         block.timestamp >= timelockActivationDate || hasFinalised == true, 
@@ -107,6 +124,47 @@ contract CreditsITO is Credits {
       );
     _;
   }
+
+
+//  ----------------------------------------------------
+//         Developer Funds Functions + Timelock
+//  ----------------------------------------------------
+
+  // Prevents developer(s) from withdrawing credits instantly after the ito has finished
+  modifier creditsTimeLock {
+    require(
+      timelockActivationDate >= timelockActivationDate + 26 weeks, 
+      "Error: Wait until timelock period is over to access functionality"
+      );
+    _;
+  }
+
+  // Allows users to view when the dev. timelock is over
+  function viewTimeLockStatus() external view returns (
+      uint256 timelockCommenced, 
+      uint256 timelockCompletition, 
+      uint256 timeRemaining
+    ) { 
+    return (
+      timelockActivationDate,
+      timelockActivationDate + 26 weeks,
+      (timelockActivationDate + 26 weeks).sub(block.timestamp)
+    );
+  }
+
+  // Allows the developer to withdraw "x" amount of  the remaining, unsold credits, or send them to the credits contract
+  function developerCreditsWithdraw(address _to, uint256 _amount) permissionRequired creditsTimeLock external {
+      users[itoContract].creditBalance = users[itoContract].creditBalance.sub(_amount);
+      users[_to].creditBalance = users[_to].creditBalance.add(_amount);
+      emit developerEthWithdrawal(_to, _amount, block.timestamp);
+  } 
+
+  // Allows the developer to withdraw a desired amount of ETH to their desired address
+  function developerEthWithdraw(address payable _to, uint256 _amount) permissionRequired external {
+      _to.transfer(_amount);
+      emit developerEthWithdrawal(_to, _amount, block.timestamp);
+  } 
+
 
 //  ----------------------------------------------------
 //                 External Functions 
@@ -117,7 +175,7 @@ contract CreditsITO is Credits {
     require(users[msg.sender].credibyteBalance != 0, "No credibytes remaining.");
     uint256 _conversionAmount = validateConversion();                                           // checks how many credits user will receive
     commenceConversion(_conversionAmount);                                                      // transfers credits to user
-    emit CredibytesRedeption(msg.sender, _conversionAmount, block.timestamp);
+    emit CredibytesRedemption(msg.sender, _conversionAmount, block.timestamp);
     return (_conversionAmount, true);
   }
 
@@ -128,20 +186,27 @@ contract CreditsITO is Credits {
     _preValidatePurchase(_beneficiary, ethAmount);                                              // validates tx isn't sending 0 wei
     uint256 credibyteAmount = _getCredibyteAmount(ethAmount);                                   // calculates the amount of credits to be created
     
-    _forwardFunds(ethAmount);                                                                    // transfers eth to itoCollector
+    _forwardFunds(ethAmount);                                                                   // transfers eth to itoCollector
     totalEthRaised = totalEthRaised.add(ethAmount);                                             // updates state: totalEthRaised
 
-    _processPurchase(_beneficiary, credibyteAmount);                                            // transfers credits to beneficiary
-
-      if(users[_beneficiary].hasParticipatedInITO != true) {                                    // adds users to hasParticipated if they haven't
-        participants.push(_beneficiary);
-        users[_beneficiary].hasParticipatedInITO = true;
-      }                         
+    _processPurchase(_beneficiary, credibyteAmount);                                            // transfers credits to beneficiary                       
     emit CredibytesPurchase(msg.sender, _beneficiary, ethAmount, credibyteAmount); 
   }
 
+  // Views the inputted user's credibyte balance
   function viewCredibyeBalance(address user) external view returns (uint256 _credibyteBalance) {
     return users[user].credibyteBalance;
+  }
+
+  // Views how many remaining credibytes there are for purchase, with the total eth value of the remaining
+  function viewRemainingCredibytesForPurchase() external view returns (
+    uint256 remainingCredibytesForPurchase, 
+    uint256 ethValueOfRemainingCredibytesForPurchase
+    ) {
+    return(
+      users[itoContract].credibyteBalance,
+      (users[itoContract].credibyteBalance.div(conversionRate_EthToCredibytes)).mul(1 ether)
+    );
   }
 
   // If unlocked, call buyCredits_forWei || If locked, revert tx.
@@ -150,6 +215,7 @@ contract CreditsITO is Credits {
       buyCredibytes_withETH(msg.sender); 
     } else { revert(); }
   }
+
 
 //  ----------------------------------------------------
 //     convertCredibytesToCredits Internal Functions 
@@ -172,7 +238,6 @@ contract CreditsITO is Credits {
       users[msg.sender].credibyteBalance != 0,
       "Error: Insufficient credibyte balance to redeem."
     );
-      // if(users[msg.sender].credibyteBalance.mod(4) == 0) {
         if(users[msg.sender].redemptionCounter == 0) {
           users[msg.sender].redemptionCounter = users[msg.sender].redemptionCounter.add(1);       // adds 1 onto the user's current redemption counter
           users[msg.sender].remainingTimeUntilNextConversion = block.timestamp + 4 weeks;         // adds 1 month until user's next redemption activation
@@ -194,10 +259,6 @@ contract CreditsITO is Credits {
             users[msg.sender].fullyConverted = true;
             return users[msg.sender].credibyteBalance;                                            // i.e. balance = 250, calculates remaining       
         }
-      // } else {
-      //     users[msg.sender].fullyConverted = true;
-      //     return currentCredibyteConversion = users[msg.sender].credibyteBalance;      
-      // }
     }
 
   function commenceConversion(uint256 _conversionAmount) internal {
@@ -209,6 +270,7 @@ contract CreditsITO is Credits {
     // users[msg.sender].creditBalance = users[msg.sender].creditBalance.add(_conversionAmount);             // gives user converted credits amount
     // totalCreditsHeld = totalCreditsSupply.sub(remainingUnheldCredits);                                    // updates total credits held
   }
+
 
 //  ----------------------------------------------------
 //       buyCredibytes_forETH Internal Functions 
@@ -233,14 +295,16 @@ contract CreditsITO is Credits {
     return ((_ethAmount.mul(conversionRate_EthToCredibytes)).div(1 ether));
   }
 
-  // Transfers eth (msg.value) to ito_Collector
+  // Transfers eth (msg.value) to developerContract
   function _forwardFunds(uint256 ethAmount) ito_Timelock internal {
-    ito_Collector.transfer(ethAmount);
+    itoContract.transfer(ethAmount);
+    emit ethDepositToDeveloper(msg.sender, msg.value, block.timestamp);
   }
 
   // Calls _deliverTokens.
   function _processPurchase(address _beneficiary, uint256 _credibyteAmount) ito_Timelock internal {
     _deliverCredibytes(_beneficiary, _credibyteAmount);
+
   }
 
   // Updates credibyte balance
@@ -249,6 +313,10 @@ contract CreditsITO is Credits {
     if(users[_beneficiary].redemptionCounter != 0 || users[_beneficiary].fullyConverted != false) {
       users[_beneficiary].redemptionCounter = 0;
       users[_beneficiary].fullyConverted = false;
+    }
+    if(users[_beneficiary].hasParticipatedInITO != true) {                                    // adds users to hasParticipated if they haven't
+      users[_beneficiary].hasParticipatedInITO = true;
+      itoTotalParticipants = itoTotalParticipants.add(1);
     }
   }
 }
