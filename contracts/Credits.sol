a955a2a3d28bf7d8ef6f8d1e80e3b48f97234b99
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2021-01-07 20:29:15 +1100

    Update Credits.sol

--- contracts/Credits.sol
+++ contracts/Credits.sol
@@ -1,14 +1,18 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "./Permissioned.sol";
-import "./ICredits.sol";
+import "./lib/Permissioned.sol";
+import "./interfaces/ICredits.sol";
 
 // ----------------------------------------------------------------------------
 //
 //                      (c) The BotNet Project 2020
 //
 // ----------------------------------------------------------------------------
+
+// TO DO...
+// [ ] ADD PERMISSION MODIFIER W/ "hasAccess" bool variable -> ONLY OBTAINABLE VIA DEPLOYER CONFIRMATION 
+// [ ] MAKE APPROVE FUNCTION THAT CONTRACTS WITH PERMISSION CAN ACCESS 
 
 contract Credits is ICredits, Permissioned {
 	using SafeMath for uint256;
@@ -20,18 +24,18 @@ contract Credits is ICredits, Permissioned {
 	string _name = "Credits";
     string _symbol = "CRDTS";
     uint8 _decimals = 18;                       
-    uint256 public initialCreditsSupply;        // supply upon deployment
-    uint256 private totalCreditsSupply;         // Credits' total supply (can be adjusted)
-    uint256 public totalCreditsHeld;	        // how many Credits are in custody of users
-    uint256 public remainingUnheldCredits;      // amount of credits that aren't owned
-    address payable creditsContract;            // the address that holds the total supply
+    uint256 public initialCreditsSupply = 20000000 * 10**uint(_decimals);         // 20,000,000 credits supply upon deployment
+    uint256 private totalCreditsSupply;                                           // Credits' total supply (can be adjusted)
+    uint256 public totalCreditsHeld;	                                          // how many Credits are in custody of users
+    uint256 public remainingUnheldCredits;                                        // amount of credits that aren't owned
+    address payable creditsContract;                                              // the address that holds the total supply
 
     constructor() {    
-        creditsContract = address(this);                                   // creditsContract = this contract address (Credits.sol)     
-        initialCreditsSupply = 1500000 * 10**uint(_decimals);              // 1,500,000 inital credits supply
+        isPaused = false;                                                  // contract is unpaused on deployment
+        creditsContract = address(this);                                   // creditsContract = this contract address (Credits.sol)    
+        users[creditsContract].hasContractAccess = true;                   // is given contract access for contractApprove()
     	totalCreditsSupply = initialCreditsSupply;                         // total credits supply = total inital credits supply
         totalCreditsHeld = 0;                                              // credits held by users = 0
-    	isPaused = false;                                                  // contract is unpaused on deployment
         users[creditsContract].creditBalance = totalCreditsSupply;         // creditsContract owns total supply
         remainingUnheldCredits = users[creditsContract].creditBalance;     // amount of credits that aren't owned = total supply
     }
@@ -57,54 +61,60 @@ contract Credits is ICredits, Permissioned {
     }
 
 //  ----------------------------------------------------
-//                 Transfer Functions 
+//                User Transfer Functions 
 //  ----------------------------------------------------
 
-    function transfer(address _to, uint _value) override external pauseFunction returns (bool success) {
-        require(users[msg.sender].creditBalance >= _value, "insufficient funds, revert");
-        _transfer(msg.sender, _to, _value);
+    function transfer(address _to, uint _amount) override external pauseFunction returns (bool success) {
+        require(users[msg.sender].creditBalance >= _amount, "insufficient funds, revert");
+        _transfer(msg.sender, _to, _amount);
         return true;
     }
     
-    function transferFrom(address _from, address _to, uint _value) override external pauseFunction returns (bool success) {
-        require(users[_from].creditBalance >= _value, "from address has insufficient funds, revert");
-        require(users[msg.sender].allowance[_from] >= _value, "insufficient allowance, revert");
+    function transferFrom(address _from, address _to, uint _amount) override external pauseFunction returns (bool success) {
+        require(users[_from].creditBalance >= _amount, "from address has insufficient funds, revert");
+        require(users[msg.sender].allowance[_from] >= _amount, "insufficient allowance, revert");
         require(users[msg.sender].allowance[_from] <= users[_from].creditBalance);
-        users[msg.sender].allowance[_from] = users[msg.sender].allowance[_from].sub(_value);
-        _transfer(_from, _to, _value);
+        users[msg.sender].allowance[_from] = users[msg.sender].allowance[_from].sub(_amount);
+        _transfer(_from, _to, _amount);
         return true;
     }
 
-    function _transfer(address _from, address _to, uint256 _value) private pauseFunction {
+    function _transfer(address _from, address _to, uint256 _amount) private pauseFunction {
         if(_from == creditsContract) {
-            users[_from].creditBalance = users[_from].creditBalance.sub(_value);
-            users[_to].creditBalance = users[_to].creditBalance.add(_value);
+            users[_from].creditBalance = users[_from].creditBalance.sub(_amount);
+            users[_to].creditBalance = users[_to].creditBalance.add(_amount);
             remainingUnheldCredits = users[owner].creditBalance;
             totalCreditsHeld = totalCreditsSupply.add(remainingUnheldCredits);
         } else if (_to == creditsContract){
-            users[_from].creditBalance = users[_from].creditBalance.sub(_value);
-            users[_to].creditBalance = users[_to].creditBalance.add(_value);
+            users[_from].creditBalance = users[_from].creditBalance.sub(_amount);
+            users[_to].creditBalance = users[_to].creditBalance.add(_amount);
             remainingUnheldCredits = users[owner].creditBalance;
             totalCreditsHeld = totalCreditsSupply.sub(remainingUnheldCredits);
         } else {
-            users[_from].creditBalance = users[_from].creditBalance.sub(_value);
-            users[_to].creditBalance = users[_to].creditBalance.add(_value);
+            users[_from].creditBalance = users[_from].creditBalance.sub(_amount);
+            users[_to].creditBalance = users[_to].creditBalance.add(_amount);
         }
-        emit Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _amount);
     }
 
 //  ----------------------------------------------------
-//                 Approve + Allowance 
+//               User Approve + Allowance 
 //  ----------------------------------------------------
 
-    function approve(address _spender, uint _value) override external pauseFunction returns (bool success) {
-    	_approve(msg.sender, _spender, _value);
+    function contractApprove(address _approver, address _approvee, uint256 _amount) contractAccess external returns (bool success) {
+        _approve(_approver, _approvee, _amount);
+        return true;
+    }
+
+    function approve(address _spender, uint _amount) override external pauseFunction returns (bool success) {
+        // require(msg.sender != creditsContract, "ERROR: Unable to set ");
+    	_approve(msg.sender, _spender, _amount);
     	return true;
     }
 
-    function _approve(address _owner, address _spender, uint _value) private {
-        users[_owner].allowance[_spender] = _value;
-        emit Approval(_owner, _spender, _value);
+    function _approve(address _owner, address _spender, uint _amount) private {
+        users[_owner].allowance[_spender] = _amount;
+        emit Approval(_owner, _spender, _amount);
     }
 
     function viewAllowance(address tokenOwner, address spender) override external pauseFunction view returns (uint remaining) {
@@ -115,12 +125,11 @@ contract Credits is ICredits, Permissioned {
 //                 Mint + Burn Credits 
 //  ----------------------------------------------------
 
-    function generateCredits(address _to, uint _amount) override external onlyOwner returns (bool success) {
-        users[_to].creditBalance = users[_to].creditBalance.add(_amount);
+    function generateCredits(uint _amount) override external onlyOwner returns (bool success) {
+        users[creditsContract].creditBalance = users[creditsContract].creditBalance.add(_amount);
         totalCreditsSupply = totalCreditsSupply.add(_amount);
-        totalCreditsHeld = totalCreditsHeld.add(_amount);
         remainingUnheldCredits = users[owner].creditBalance;              
-        emit generatedCredits(totalCreditsSupply, _to, _amount);
+        emit generatedCredits(totalCreditsSupply, creditsContract, _amount);
         return true;
     }
     function deleteCredits(uint _amount) override external onlyOwner returns (bool success) {
