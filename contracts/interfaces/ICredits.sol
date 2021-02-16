47c40bed251b4d458700785e014db89efef901f5
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2021-02-16 21:27:24 +1100

    Update ICredits.sol

--- contracts/interfaces/ICredits.sol
+++ contracts/interfaces/ICredits.sol
@@ -6,9 +6,8 @@ import "./ERC20.sol";
 /// @notice CreditsInterface = ERC20 + mint + burn. (c) The BotNet Project 2020
 
 interface ICredits is ERC20 {
-	function generateCredits(uint _amount) external returns (bool success);
 	function deleteCredits(uint _amount) external returns (bool success);
 
-	event generatedCredits(uint totalSupply, address generatedTo, uint amountGenerated);
+	event generatedCredits(uint indexed currentTotalSupply, address indexed generatedTo, uint indexed amountGenerated);
 	event deletedCredits(uint totalSupply, uint amountDeleted);
 }
