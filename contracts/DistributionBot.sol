7ed13b7e4feeca1ab195a22a509b13c4ad188d3d
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2021-04-01 12:16:53 +1100

    Update DistributionBot.sol

--- contracts/DistributionBot.sol
+++ contracts/DistributionBot.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.7.0;
 
 import "./Credits.sol";
+import "./lib/Permissioned.sol";
 
 // ----------------------------------------------------------------------------
 //
@@ -11,10 +12,31 @@ import "./Credits.sol";
 
 ///@dev add decryptEnigmaPortion()
 
-contract DistributionBot is Credits {
+contract DistributionBot is Permissioned {
     using SafeMath for uint256;
 
     Credits credits;
+
+    /// @dev Enigma entrance fee
+    private uint256 _enigmaFee;
+
+    /// @dev Total funds in Enigma
+    private uint256 _enigmaBalance;
+
+    /// @dev Total enigma participants
+    uint256 totalEnigmaParticipants;
+
+    /// @dev Addresses participating in Enigma
+    address[] enigmaParticipants;
+
+    /// @dev Is user in enigma
+    bool inEnigma;
+
+    /// @dev Block that user joined Enigma
+    mapping(address => uint256[]) joinedEnigma;
+
+    /// @dev Block that user left engima
+    mapping(address => uint256[]) leftEnigma;
 
     event userJoiningEngima(
         address indexed userJoining, 
@@ -35,66 +57,73 @@ contract DistributionBot is Credits {
     );
 
     constructor() {
+        if(msg.sender == owner) {
+            grantContractAccess(address(this));
+        } 
         uint enigmaFee = 1000;
         _enigmaFee = enigmaFee;
     }
 
     /// @notice allows the caller to enter the Enigma for a fee.
-    /// @return entranceFee                 : the fee price required to enter the Enigma pool.
-    /// @return success                     : whether or not the transaction to leave was a success
     function enterEnigma() external pauseFunction returns (uint entranceFee, bool success) {
         require(users[msg.sender].creditBalance >= _enigmaFee, "insufficient funds to enter Enigma");
         require(users[msg.sender].inEnigma == false, "already in Enigma");
+
         credits.transfer(address(credits), _enigmaFee);
-        users[msg.sender].joinedEnigma = block.timestamp;
-        users[msg.sender].leftEnigma = 0;
-        users[msg.sender].inEnigma = true;
+        msg.sender.joinedEnigma = block.timestamp;
+        msg.sender.leftEnigma = 0;
+        msg.sender.inEnigma = true;
         enigmaParticipants.push(msg.sender);
         totalEnigmaParticipants = totalEnigmaParticipants.add(1);
+
         emit userJoiningEngima(msg.sender, _enigmaFee, block.timestamp);
         return (_enigmaFee, true);
     }
 
     /// @notice allows the user to leave the Enigma pool.
-    /// @return success                      : whether or not the transaction to leave was a success
     function leaveEnigma() external pauseFunction returns (bool success) {
-        require(users[msg.sender].inEnigma == true, "unable to leave when you have not joined the Enigma");
+        require(msg.sender.inEnigma == true, "unable to leave when you have not joined the Enigma");
+
         users[msg.sender].inEnigma = false;
         enigmaParticipants.pop();
         totalEnigmaParticipants = totalEnigmaParticipants.add(1);
         users[msg.sender].leftEnigma = block.timestamp;
+
         return true;
     }
 
-    /// @notice allows the owner to update the Enigma enterance fee.
-    /// @param _newFee                       : the updated fee for entering the Enigma.
-    /// @return updatedFee                   : returns the new fee, set by the owner.
-    /// @return success                      : whether or not the transaction to leave was a success
+    /// @notice Allows the owner to update the Enigma enterance fee.
     function updateFee(uint _newFee) external onlyOwner returns (uint updatedFee, bool success) {
         _enigmaFee = _newFee;
         emit newEntranceFee(_newFee, block.timestamp);
         return (_enigmaFee, true);
     }
 
-    /// @notice allows the user to view the entranaceFee & Enigma's balance.
-    /// @return entranceFee                 : the fee price required to enter the Enigma pool.
-    /// @return enigmaBalance               : the total amount of Credits stored in the Enigma.
-    function viewEnigma() external view returns (uint entranceFee, uint enigmaBalance) {
-        return (_enigmaFee, _enigmaBalance);
+    /// @notice Allows the user to view the entranaceFee & Enigma's balance.
+    function getEntranceFee() external view returns (uint256) {
+        return (_enigmaFee);
+    }
+
+    function getEnigmaBalance() external view returns (uint256) {
+        return (_enigmaBalance);
+    }
+
+    function getParticipants() external view returns (uint256, address[]) {
+        return (totalEnigmaParticipants, enigmaParticipants[]);
     }
 
     /// @notice allows owner to decrypt funds within Enigma which then distributes to members of the Enigma equally. 
-    /// @dev potentially making the users able to call this function.
-    /// @return totalRecipients             : the total amount of Engima share recipients.
-    /// @return decryptedCreditsPerPerson   : the share each member gets (calculated via: total amount of Credits divided by the amount of Engima members). 
-    function decryptEnigma() external pauseFunction onlyOwner returns (uint256 totalRecipients, uint256 decryptedCreditsPerPerson) {
+    function decryptEnigma() external pauseFunction returns (uint256 totalRecipients, uint256 decryptedCreditsPerPerson) {
         require(totalEnigmaParticipants != 0, "no participants in Enigma for distribution");
+
         uint _creditsPerPerson = (_enigmaBalance.div(enigmaParticipants.length));
-            for(uint i = 0; enigmaParticipants.length > i; i++) {
+            for(uint i = 0; enigmaParticipants.length > i;) {
                 address n = enigmaParticipants[i];
                 credits.approve(address(this), _creditsPerPerson);
-                credits.transferFrom(creditsContract, n, _creditsPerPerson);
+                credits.transferFrom(address(credits), n, _creditsPerPerson);
+                i++
             }
+            
         emit enigmaDecryption(totalEnigmaParticipants, _creditsPerPerson);
         return (totalEnigmaParticipants, _creditsPerPerson);
     }
